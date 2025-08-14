42ea4b6a5271ff266250d611860765405eb8a471
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-08-14 22:06:57 +0300

    Initial commit

--- parcel and courier/frontend/vite.config.ts
+++ parcel and courier/frontend/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ✅ ensures assets load correctly on Vercel
   plugins: [react()],
   resolve: {
     alias: {
