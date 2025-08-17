be8a6b301199e6145ff511daa95b5a3ee6425685
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-08-17 13:04:34 +0300

    Working on change address

--- parcel and courier/frontend/src/supabase/supabaseRealTime.ts
+++ parcel and courier/frontend/src/supabase/supabaseRealTime.ts
@@ -10,18 +10,15 @@ import { type TAppDispatch } from "@/app/store";
 export const initRealtime = (dispatch: TAppDispatch) => {
   const channel = supabase.channel("address_change_request");
 
-  // INSERT
   channel.on("broadcast", { event: "INSERT" }, ({ payload }) => {
     console.log(payload);
     dispatch(insertSyncRequest(payload));
   });
 
-  // UPDATE
   channel.on("broadcast", { event: "UPDATE" }, ({ payload }) => {
     dispatch(updateSyncRequest(payload));
   });
 
-  // DELETE
   channel.on("broadcast", { event: "DELETE" }, ({ payload }) => {
     dispatch(removeSyncRequest(payload.parcel_id));
   });
@@ -31,7 +28,6 @@ export const initRealtime = (dispatch: TAppDispatch) => {
   return channel;
 };
 
-// Helper to send broadcasts
 export const sendRealtimeEvent = (
   event: "INSERT" | "UPDATE" | "DELETE",
   data: unknown
